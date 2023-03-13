3848811e280d31df7ce9fc3f8b7c9c280d9a4957
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-03-13 23:30:00 +0100

    fix: Docs changed to postman

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -9,7 +9,7 @@ const { fetchAllComments } = require("../controllers/comment.controller")
 
 router.get('/users/:@username/posts/', testmiddleware, fetchAllPosts)
 router.get('/users/:@username/posts/:id/comments/', testmiddleware, fetchAllComments)
-router.get('/docs', (req, res) => res.redirect('https://post-it-q0g4.onrender.com'))
+router.get('/docs', (req, res) => res.redirect('https://documenter.getpostman.com/view/19026826/2s93JtRPcU'))
 
 router.use('/users', testmiddleware, userRouter)
 router.use('/users/:id/posts', testmiddleware, postRouter)
